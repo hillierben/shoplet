13124217d002df8c67a3e54bcd4e03fe6d3a2446
hillierben <hillierben@gmail.com>
2023-11-03 06:15:12 +0000

    latest

--- app/components/Paginate.jsx
+++ app/components/Paginate.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 export default function Paginate() {
   return (
     <div className='pagi-div'>
-      <Link href='#' className='pagi-btn'>Prev</Link>
+      <Link href='#' className='pagi-btn'>&#8592;</Link>
       <p className='pagi-pge'>Page 1 of 1</p>
-      <Link href='#' className='pagi-btn'>Next</Link>
+      <Link href='#' className='pagi-btn'>&rarr;</Link>
     </div>
   )
 }
